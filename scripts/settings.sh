eaca0df4897a76975576eb81c0412651b96be502
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-29 17:41:38 -0700

    Inject environment variables into app settings

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 SETTINGS_FILE=dist/settings.js
 SETTINGS_VARIABLE=AppSettings
 
 cat > $SETTINGS_FILE << EndOfFile
 var $SETTINGS_VARIABLE = {
-  API_URL: 'http://localhost:3000'
+  API_URL: '${API_URL:-http://localhost:3000}'
 };
 EndOfFile
